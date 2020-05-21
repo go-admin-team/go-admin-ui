cb63479221803ae7b0be6b3e9e73e53f3c788581
colynn.liu <colynn.liu@gmail.com>
2020-05-21 19:15:20 +0800

    add parseJwt func

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -13,3 +13,14 @@ export function setToken(token) {
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
+
+export function parseJwt(token) {
+  var base64Url = token.split('.')[1]
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+  }).join(''))
+
+  return JSON.parse(jsonPayload)
+}
+
