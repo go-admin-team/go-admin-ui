a60554c997c2d95c28a15e23efdafce04ddfc547
zhangwenjian <zwj777@live.com>
2020-10-14 10:32:13 +0800

    update：升级了sysconfig 的基本方法

--- src/api/system/config.js
+++ src/api/system/config.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询参数列表
 export function listConfig(query) {
   return request({
-    url: '/api/v1/configList',
+    url: '/api/v1/config',
     method: 'get',
     params: query
   })
@@ -20,8 +20,9 @@ export function getConfig(configId) {
 // 根据参数键名查询参数值
 export function getConfigKey(configKey) {
   return request({
-    url: '/api/v1/configKey/' + configKey,
-    method: 'get'
+    url: '/api/v1/configKey',
+    method: 'post',
+    data: { 'configKey': configKey }
   })
 }
 
@@ -37,7 +38,7 @@ export function addConfig(data) {
 // 修改参数配置
 export function updateConfig(data) {
   return request({
-    url: '/api/v1/config',
+    url: '/api/v1/config/' + data.ID,
     method: 'put',
     data: data
   })
@@ -51,11 +52,3 @@ export function delConfig(configId) {
   })
 }
 
-// 导出参数
-export function exportConfig(query) {
-  return request({
-    url: '/api/v1/config/export',
-    method: 'get',
-    params: query
-  })
-}
