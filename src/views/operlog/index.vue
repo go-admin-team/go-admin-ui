6049b3919e5d0b6151046bc54ec8221a8fa967c4
zhangwenjian <zwj777@live.com>
2021-01-11 16:28:29 +0800

    perf👌 : 优化操作日志功能

--- src/views/operlog/index.vue
+++ src/views/operlog/index.vue
@@ -76,9 +76,8 @@
         <el-table v-loading="loading" :data="list" @selection-change="handleSelectionChange">
           <el-table-column type="selection" width="55" align="center" />
           <el-table-column label="日志编号" width="80" align="center" prop="ID" />
-          <el-table-column label="系统模块" align="center" prop="title" :show-overflow-tooltip="true" />
-          <el-table-column label="操作类型" width="80" align="center" prop="businessType" :formatter="typeFormat" :show-overflow-tooltip="true" />
           <el-table-column label="请求方式" width="80" align="center" prop="requestMethod" :show-overflow-tooltip="true" />
+          <el-table-column label="请求地址" align="center" prop="operUrl" :show-overflow-tooltip="true" />
           <el-table-column label="操作人员" align="center" prop="operName" :show-overflow-tooltip="true" />
           <el-table-column label="主机" align="center" prop="operIp" width="130" :show-overflow-tooltip="true" />
           <el-table-column label="操作地点" align="center" prop="operLocation" :show-overflow-tooltip="true" />
@@ -124,7 +123,7 @@
                 <el-form-item label="请求方式：">{{ form.requestMethod }}</el-form-item>
               </el-col>
               <el-col :span="12">
-                <el-form-item label="耗时：">{{ form.latencyime }}</el-form-item>
+                <el-form-item label="耗时：">{{ form.latencyTime }}</el-form-item>
               </el-col>
               <el-col :span="24">
                 <el-form-item label="操作方法：">{{ form.method }}</el-form-item>
@@ -137,8 +136,8 @@
               </el-col>
               <el-col :span="12">
                 <el-form-item label="操作状态：">
-                  <div v-if="form.status === 0">正常</div>
-                  <div v-else-if="form.status === 1">失败</div>
+                  <div v-if="form.status === '2'">正常</div>
+                  <div v-else-if="form.status === '1'">失败</div>
                 </el-form-item>
               </el-col>
               <el-col :span="12">
