af15c5b8c3cf2022acf5d159176bdad7a45d70e6
wenjianzhang <zwj777@live.com>
2021-01-11 21:44:43 +0800

    Update index.vue

--- src/views/dict/index.vue
+++ src/views/dict/index.vue
@@ -233,7 +233,7 @@ export default {
     },
     // 字典状态字典翻译
     statusFormat(row, column) {
-      return this.selectDictLabel(this.statusOptions, row.status)
+      return this.selectDictLabel(this.statusOptions, parseInt(row.status))
     },
     // 取消按钮
     cancel() {
