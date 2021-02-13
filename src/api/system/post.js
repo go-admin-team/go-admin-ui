cb0179642e7ff351aa4371334041d59f3e242b28
wanghaima <wanghaima@baimaohui.net>
2021-02-14 01:20:52 +0800

    修改图片上传的bug,添加返回本地图片路径 修复部门和岗位的404的bug

--- src/api/system/post.js
+++ src/api/system/post.js
@@ -3,7 +3,8 @@ import request from '@/utils/request'
 // 查询岗位列表
 export function listPost(query) {
   return request({
-    url: '/api/v1/postlist',
+    // url: '/api/v1/postlist',
+    url: '/api/v1/post',
     method: 'get',
     params: query
   })
