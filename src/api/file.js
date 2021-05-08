f8b0c1e17399dee2334de0fd134524c7807dd756
zhangwenjian <zwj777@live.com>
2021-05-08 09:34:49 +0800

    feat✨ 调整文件管理接口路径

--- src/api/file.js
+++ src/api/file.js
@@ -2,53 +2,53 @@ import request from '@/utils/request'
 import { data } from 'autoprefixer'
 
 export const sysfiledirList = data => request({
-  url: '/api/v1/sysfiledir',
+  url: '/api/v1/file-dir',
   method: 'GET',
   data
 })
 
 export const sysfiledirAcionAdd = data => request({
-  url: '/api/v1/sysfiledir',
+  url: '/api/v1/file-dir',
   method: 'POST',
   data
 })
 
 export const sysfiledirAcionEdit = data => request({
-  url: '/api/v1/sysfiledir/' + data.id,
+  url: '/api/v1/file-dir/' + data.id,
   method: 'PUT',
   data
 })
 
 export const sysfiledirAcionDel = data => request({
-  url: '/api/v1/sysfiledir/' + data,
+  url: '/api/v1/file-dir/' + data,
   method: 'DELETE'
 })
 
 export const sysfileinfoList = data => request({
-  url: `/api/v1/sysfileinfo?pId=${data.pId}&pageSize=${data.pageSize}&pageIndex=${data.pageIndex}`,
+  url: `/api/v1/file-info?pId=${data.pId}&pageSize=${data.pageSize}&pageIndex=${data.pageIndex}`,
   method: 'GET',
   data
 })
 
 export const sysfileinfo = id => request({
-  url: '/api/v1/sysfileinfo/' + id,
+  url: '/api/v1/file-info/' + id,
   method: 'GET'
 })
 
 export const sysfileinfoAdd = data => request({
-  url: '/api/v1/sysfileinfo',
+  url: '/api/v1/file-info',
   method: 'POST',
   data
 })
 
 export const sysfileinfoEdit = data => request({
-  url: '/api/v1/sysfileinfo/' + data.id,
+  url: '/api/v1/file-info/' + data.id,
   method: 'put',
   data
 })
 
 export const sysfileinfoDelete = id => request({
-  url: '/api/v1/sysfileinfo/' + id,
+  url: '/api/v1/file-info/' + id,
   method: 'DELETE',
   data
 })
