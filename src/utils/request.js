d08daab8b3174883a9ed2864a39114c6bda1de31
wenjianzhang <zwj777@live.com>
2023-12-11 08:55:14 +0800

    update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,7 +5,7 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseUrl: process.env.VUE_APP_BASE_API, // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 10000 // request timeout
 })
