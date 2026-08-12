77dd9ebaca41d9e215f7be2e367f8c155cf01a9c
zhangwenjian <zwj777@live.com>
2026-08-12 11:11:22 +0800

    fix🐛: 全局注册 Element Plus 图标组件
    模板中的 <el-icon><User /></el-icon> 依赖图标组件可被解析，此前未做
    全局注册，Vue 将其视为未知 HTML 标签，渲染出 <user></user> 而无任何
    SVG 输出，登录页与各表单的前缀图标全部不可见。

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import Cookies from 'js-cookie'
 import 'normalize.css/normalize.css' // a modern alternative to CSS resets
 
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
@@ -80,6 +81,12 @@ app.component('Pagination', Pagination)
 app.component('BasicLayout', BasicLayout)
 app.component('CodeEditor', Codemirror)
 app.component('SvgIcon', SvgIcon)
+
+// 全局注册 Element Plus 图标：模板中的 <el-icon><Xxx /></el-icon> 需要
+// 图标组件可被解析，否则会被当作未知 HTML 标签而不渲染任何内容
+for (const [name, comp] of Object.entries(ElementPlusIconsVue)) {
+  app.component(name, comp)
+}
 
 // 注册插件
 app.use(store)
